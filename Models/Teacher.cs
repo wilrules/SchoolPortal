faf27225c3e39a506d6e0199871c42a58c9dc590
doubletwos <wilsonwales@gmail.com>
2020-02-16 16:53:12 +0000

    16/02 Teachers Details Page Sorted

--- Models/Teacher.cs
+++ Models/Teacher.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
@@ -9,14 +10,19 @@ namespace SchoolPortal.Models
     public class Teacher
     {
         [ForeignKey("Year")]
+        [Display(Name = "Class")]
         public int TeacherId { get; set; }
 
+        [Display(Name ="Title")]
         public string Title { get; set; }
 
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
 
-        public string LastName { get; set; }    
+        [Display(Name = "Surname")]
+        public string LastName { get; set; }
 
+        [Display(Name = "Class")]
         public Year Year { get; set; }
     }
 }
